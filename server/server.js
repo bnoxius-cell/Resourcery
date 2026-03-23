d7cf807758d23480ebe912dfd551a42b074b571f
bnoxius-cell <idevadrian@gmail.com>
2026-03-23 22:54:42 +0800

    setup db config

--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import cookieParser from 'cookie-parser'
+
+import connectDB from './config/db.js';
 
 const app = express();
-app.use(cors()); 
 app.use(express.json()); 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+})); 
+app.use(cookieParser());
 
 const uri = process.env.MONGO_URI; 
-
-mongoose.connect(uri)
-  .then(() => {
-    console.log('Successfully connected to MongoDB Atlas!');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
 
 app.get('/', (req, res) => {
   res.send('The Resourcery API is running!');
 });
 
+// declare port and start server
 const PORT = process.env.PORT || 5000;
+connectDB()
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
